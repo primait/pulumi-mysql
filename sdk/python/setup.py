838d6308687643934249ef4c54685f774efaea36
Luca Zecca <l.zecca78@gmail.com>
2024-10-23 12:43:17 +0200

    Bump version: v0.0.10-2-g47b2ef4 → 0.0.11

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -8,7 +8,7 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
-VERSION = "0.0.10"
+VERSION = "0.0.11"
 def readme():
     try:
         with open('README.md', encoding='utf-8') as f:
